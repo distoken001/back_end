8e4e1406de2ba55789fa7bcd35ffa3e4090770f7
SouvernirLiu <songnian.liu@outlook.com>
2023-04-22 15:10:08 +0800

    增加配置

---
+++ deploy_dev.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# 设置项目名称和发布目录
+project_name="deMarketService"
+publish_directory="./publish"
+
+# 构建项目
+echo "Building the project..."
+dotnet build --configuration Release
+
+# 发布项目
+echo "Publishing the project..."
+dotnet publish --configuration Release --output $publish_directory
+
+# 复制 PM2 配置文件到发布目录
+echo "Copying PM2 config file to the publish directory..."
+cp  ./src/DeMarketService/pm2.config.js $publish_directory
+
+# 进入发布目录
+cd $publish_directory
+
+# 使用 PM2 运行项目
+echo "Starting the application with PM2..."
+pm2 start pm2.config.js --env development
