41667cdc8a8c23d5366dd215b245d44d50c8e585
SouvernirLiu <songnian.liu@outlook.com>
2023-04-24 22:51:53 +0800

    Revert "注释"
    This reverts commit 5d68c96c20356fb391182331aeb13e0612e3e7d9.

--- src/DeMarketService/Filters/TokenFilter.cs
+++ src/DeMarketService/Filters/TokenFilter.cs
@@ -27,43 +27,43 @@ namespace deMarketService.Proxies
         public async Task OnAuthorizationAsync(AuthorizationFilterContext context)
         {
             var path = context.HttpContext.Request.Path.Value.ToLower();
-            if (!path.Equals("/api/user/login")) { }
-            //{
-            //    var token = context.HttpContext.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
-            //    var chain = context.HttpContext.Request.Headers["chain"].FirstOrDefault();
-            //    if (token == null)
-            //    {
-            //        if (_requireAuth)
-            //        {
-            //            context.Result = new JsonResult(new { message = "Authorization header not found" }) { StatusCode = 401 };
-            //        }
-            //        return;
-            //    }
+            if (!path.Equals("/api/user/login"))
+            {
+                var token = context.HttpContext.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+                var chain = context.HttpContext.Request.Headers["chain"].FirstOrDefault();
+                if (token == null)
+                {
+                    if (_requireAuth)
+                    {
+                        context.Result = new JsonResult(new { message = "Authorization header not found" }) { StatusCode = 401 };
+                    }
+                    return;
+                }
 
-            //    try
-            //    {
-            //        var tokenHandler = new JwtSecurityTokenHandler();
-            //        var key = Encoding.UTF8.GetBytes(StringConstant.secretKey);
-            //        tokenHandler.ValidateToken(token, new TokenValidationParameters
-            //        {
-            //            ValidateIssuer = true,
-            //            ValidateAudience = true,
-            //            ValidateLifetime = true,
-            //            ValidateIssuerSigningKey = true,
-            //            ValidIssuer = StringConstant.issuer,
-            //            ValidAudience = StringConstant.audience,
-            //            IssuerSigningKey = new SymmetricSecurityKey(key)
-            //        }, out SecurityToken validatedToken);
+                try
+                {
+                    var tokenHandler = new JwtSecurityTokenHandler();
+                    var key = Encoding.UTF8.GetBytes(StringConstant.secretKey);
+                    tokenHandler.ValidateToken(token, new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = StringConstant.issuer,
+                        ValidAudience = StringConstant.audience,
+                        IssuerSigningKey = new SymmetricSecurityKey(key)
+                    }, out SecurityToken validatedToken);
 
-            //        var jwtToken = (JwtSecurityToken)validatedToken;
-            //        var identity = new ClaimsIdentity(jwtToken.Claims);
-            //        identity.AddClaim(new Claim("login_chain", chain));
-            //        context.HttpContext.User = new ClaimsPrincipal(identity);
-            //    }
-            //    catch
-            //    {
-            //        context.Result = new JsonResult(new { message = "Invalid token" }) { StatusCode = 401 };
-            //    }
+                    var jwtToken = (JwtSecurityToken)validatedToken;
+                    var identity = new ClaimsIdentity(jwtToken.Claims);
+                    identity.AddClaim(new Claim("login_chain", chain));
+                    context.HttpContext.User = new ClaimsPrincipal(identity);
+                }
+                catch
+                {
+                    context.Result = new JsonResult(new { message = "Invalid token" }) { StatusCode = 401 };
+                }
             }
         }
     }
