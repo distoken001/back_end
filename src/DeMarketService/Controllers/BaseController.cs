6c05109b94616a5cab2f5886dfeb4fc50a941764
Chao.Wang <Chao.Wang@G17DSK3>
2023-04-24 16:49:27 +0800

    公共请求头获取链ID

--- src/DeMarketService/Controllers/BaseController.cs
+++ src/DeMarketService/Controllers/BaseController.cs
@@ -5,6 +5,9 @@ namespace deMarketService.Controllers
 {
     public class BaseController : Controller
     {
+        /// <summary>
+        /// 当前钱包地址
+        /// </summary>
         public string CurrentLoginAddress
         {
             get
@@ -12,6 +15,16 @@ namespace deMarketService.Controllers
                 var u = User.Claims.FirstOrDefault(x => x.Type == "address")?.Value;
                 if (string.IsNullOrEmpty(u)) return string.Empty;
                 return u;
+            }
+        }
+        /// <summary>
+        /// 链ID
+        /// </summary>
+        public string ChainId
+        {
+            get
+            {
+                return HttpContext.Request.Headers["chain_id"];
             }
         }
     }
