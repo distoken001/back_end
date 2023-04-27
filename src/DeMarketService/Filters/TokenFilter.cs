4d27f7ddb5f465e26332769f5103b6681d2d9e5f
SouvernirLiu <songnian.liu@outlook.com>
2023-04-28 04:19:05 +0800

    Update TokenFilter.cs

--- src/DeMarketService/Filters/TokenFilter.cs
+++ src/DeMarketService/Filters/TokenFilter.cs
@@ -30,7 +30,7 @@ namespace deMarketService.Proxies
             if (!path.Equals("/api/user/login"))
             {
                 var token = context.HttpContext.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
-                var chainId = context.HttpContext.Request.Headers["chain_id"].FirstOrDefault();
+                //var chainId = context.HttpContext.Request.Headers["chain_id"].FirstOrDefault();
                 if (token == null)
                 {
                     if (_requireAuth)
