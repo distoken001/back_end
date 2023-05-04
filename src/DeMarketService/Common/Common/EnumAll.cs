fba0596733436a7506b4f4ac76b664ce28fd5e3b
SouvernirLiu <songnian.liu@outlook.com>
2023-05-05 00:51:57 +0800

    1

--- src/DeMarketService/Common/Common/EnumAll.cs
+++ src/DeMarketService/Common/Common/EnumAll.cs
@@ -169,7 +169,7 @@
     public enum ChainEnum : int
     {
         MainNet = 1,
-        ArbitrumOne = 42167,
+        ArbitrumOne = 42161,
         optimisticEthereum = 10,
         Polygon = 137,
         Ropsten = 3,
