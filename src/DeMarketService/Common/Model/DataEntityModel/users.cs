c2f01118f4d84f1b6e49e707d8203983cf580da7
SouvernirLiu <songnian.liu@outlook.com>
2023-05-04 15:59:15 +0800

    1

--- src/DeMarketService/Common/Model/DataEntityModel/users.cs
+++ src/DeMarketService/Common/Model/DataEntityModel/users.cs
@@ -67,6 +67,10 @@ namespace deMarketService.Common.Model.DataEntityModel
          * 用户登录ip
          */
         public String ip { get; set; }
+        /// <summary>
+        /// 链id
+        /// </summary>
         public ChainEnum chain_id { get; set; }
+
     }
 }
