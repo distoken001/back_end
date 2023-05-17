94f503d90e8e647e1834b68a8f6cf53abded28ad
SouvernirLiu <songnian.liu@outlook.com>
2023-05-17 17:04:02 +0800

    1

--- src/DeMarketService/Controllers/NoticeController.cs
+++ src/DeMarketService/Controllers/NoticeController.cs
@@ -8,11 +8,20 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using deMarketService.Common.Model.HttpApiModel.RequestModel;
 using System.Net;
-using System.Net.Mail;
 using deMarketService.Common.Common;
 using deMarketService.Common.Model.DataEntityModel;
 using Microsoft.EntityFrameworkCore;
 using deMarketService.Common.Model;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Auth.OAuth2.Flows;
+using Google.Apis.Auth.OAuth2.Responses;
+using Google.Apis.Services;
+using MimeKit;
+using MailKit;
+using MailKit.Security;
+using Google.Apis.Util.Store;
+using System.Threading;
+using System.Net.Mail;
 
 namespace deMarketService.Controllers
 {
@@ -80,6 +89,56 @@ namespace deMarketService.Controllers
             {
                 Console.WriteLine("Exception caught in SendEmail(): {0}", ex.ToString());
                 return Json(new WebApiResult(1, "发送失败"));
+            }
+            return Json(new WebApiResult(1, "发送成功"));
+        }
+
+        /// <summary>
+        /// 放弃
+        /// </summary>
+        /// <param name = "req" ></ param >
+        /// < returns ></ returns >
+        [HttpPost("fdf")]
+        public async Task<JsonResult> fdf([FromBody] SendEmailRequest request)
+        {
+            // 从Google Cloud Console获取的客户端ID和客户端密钥
+            string clientId = "180806267274-o94k1fq1209obmhtedgngffv35pe56bc.apps.googleusercontent.com";
+            string clientSecret = "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";
+
+            // 你的Gmail地址
+            string senderEmail = "demarkethub@gmail.com";
+            string recipientEmail = "457456365@qq.com";
+            var clientSecrets = new ClientSecrets
+            {
+                ClientId = clientId,
+                ClientSecret = clientSecret
+            };
+
+            var codeFlow = new GoogleAuthorizationCodeFlow(new GoogleAuthorizationCodeFlow.Initializer
+            {
+                DataStore = new FileDataStore("Token"),
+                Scopes = new[] { "https://mail.google.com/" },
+                ClientSecrets = clientSecrets
+            });
+
+            var codeReceiver = new LocalServerCodeReceiver();
+            var authCode = new AuthorizationCodeInstalledApp(codeFlow, codeReceiver);
+            var credential = authCode.AuthorizeAsync("user", CancellationToken.None).Result;
+
+            using (var client = new MailKit.Net.Smtp.SmtpClient())
+            {
+                client.Connect("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
+                var oauth2 = new SaslMechanismOAuth2(senderEmail, credential.Token.AccessToken);
+                client.Authenticate(oauth2);
+
+                var message = new MimeMessage();
+                message.From.Add(new MailboxAddress("Your Name", senderEmail));
+                message.To.Add(new MailboxAddress("Recipient Name", recipientEmail));
+                message.Subject = "邮件主题";
+                message.Body = new TextPart("plain") { Text = "邮件内容" };
+
+                client.Send(message);
+                client.Disconnect(true);
             }
             return Json(new WebApiResult(1, "发送成功"));
         }
