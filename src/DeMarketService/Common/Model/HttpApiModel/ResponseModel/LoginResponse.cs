b2219d9d3f1f1aab8ad35063a790b590346b9339
SouvernirLiu <songnian.liu@outlook.com>
2023-08-04 17:33:52 +0800

    Update LoginResponse.cs

--- src/DeMarketService/Common/Model/HttpApiModel/ResponseModel/LoginResponse.cs
+++ src/DeMarketService/Common/Model/HttpApiModel/ResponseModel/LoginResponse.cs
@@ -7,11 +7,29 @@ namespace deMarketService.Common.Model.HttpApiModel.ResponseModel
 {
     public class LoginResponse
     {
+        /// <summary>
+        /// token
+        /// </summary>
         public string token { get; set; }
+        /// <summary>
+        /// 头像
+        /// </summary>
         public string avatar { get; set; }
+        /// <summary>
+        /// 昵称
+        /// </summary>
         public string nick_name { get; set; }
+        /// <summary>
+        /// 邮箱
+        /// </summary>
         public string email { get; set; }
+        /// <summary>
+        /// 是否第一次登录
+        /// </summary>
         public bool is_first { get; set; }
+        /// <summary>
+        /// 持有的nft 
+        /// </summary>
         public int[] nfts { get; set; }
     }
 }
