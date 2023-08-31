fd45e56a36d114c85155fd6a940ecf0f19279381
SouvernirLiu <songnian.liu@outlook.com>
2023-08-31 16:21:59 +0800

    Update UserController.cs

--- src/DeMarketService/Controllers/UserController.cs
+++ src/DeMarketService/Controllers/UserController.cs
@@ -96,6 +96,10 @@ namespace deMarketService.Controllers
             var token = TokenHelper.GenerateToken(StringConstant.secretKey, StringConstant.issuer, StringConstant.audience, 1, userClaims);
             var user_nfts = _mySqlMasterDbContext.user_nft.Where(a => a.address.Equals(req.address) && a.status == 1).ToList();
             var nfts = user_nfts.Select(a => a.nft).ToArray();
+            if (nfts.Length > 0)
+            {
+                users.avatar = "https://api-dev.demarket.io/docs/" + nfts[0] + ".png";
+            }
             return new WebApiResult(1, "登录成功", new LoginResponse { token = token, avatar = users.avatar, nick_name = users.nick_name, email = users.email, is_first = is_first, nfts = nfts });
         }
         /// <summary>
