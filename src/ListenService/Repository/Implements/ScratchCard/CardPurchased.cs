0b8028d79327968839e00d165e871f8e75dcbcad
SouvernirLiu <songnian.liu@outlook.com>
2023-11-28 16:26:05 +0800

    1

--- src/ListenService/Repository/Implements/ScratchCard/CardPurchased.cs
+++ src/ListenService/Repository/Implements/ScratchCard/CardPurchased.cs
@@ -12,6 +12,7 @@ using Nethereum.RPC.Eth.Subscriptions;
 using Nethereum.RPC.Reactive.Eth.Subscriptions;
 using Newtonsoft.Json;
 using System;
+using System.Net.WebSockets;
 using System.Numerics;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
@@ -31,53 +32,66 @@ namespace ListenService.Repository.Implements
         {
             try
             {
-                using (var _client = new StreamingWebSocketClient(nodeUrl))
-                {
-                    try
-                    {
-                        var _subscription = new EthLogsObservableSubscription(_client);
-                        var cardPurchased = Event<CardPurchasedEventDTO>.GetEventABI().CreateFilterInput();
+                var _client = new StreamingWebSocketClient(nodeUrl);
 
-                        _subscription.GetSubscriptionDataResponsesAsObservable().Subscribe(log =>
+
+                Console.WriteLine("状态" + _client.WebSocketState);
+                try
+                {
+                    var _subscription = new EthLogsObservableSubscription(_client);
+                    var cardPurchased = Event<CardPurchasedEventDTO>.GetEventABI().CreateFilterInput();
+
+                    _subscription.GetSubscriptionDataResponsesAsObservable().Subscribe(log =>
+                    {
+                        Console.WriteLine("CardPurchased监听到了！");
+                        // decode the log into a typed event log
+                        var decoded = Event<CardPurchasedEventDTO>.DecodeEvent(log);
+                        if (decoded != null && log.Address.Equals(contractAddress, StringComparison.OrdinalIgnoreCase))
                         {
-                            Console.WriteLine("CardPurchased监听到了！");
-                            // decode the log into a typed event log
-                            var decoded = Event<CardPurchasedEventDTO>.DecodeEvent(log);
-                            if (decoded != null && log.Address.Equals(contractAddress, StringComparison.OrdinalIgnoreCase))
+                            var card = _masterDbContext.card_type.Where(a => a.type == decoded.Event.CardType && a.chain_id == chain_id).FirstOrDefault();
+                            var token = _masterDbContext.chain_tokens.Where(a => a.token_address.Equals(card.token) && a.chain_id == card.chain_id).FirstOrDefault();
+                            var cardNotOpened = _masterDbContext.card_not_opened.Where(a => a.buyer.Equals(decoded.Event.User) && a.card_type.Equals(card.type) && a.contract.Equals(log.Address) && a.token.Equals(token.token_address)).FirstOrDefault();
+                            if (cardNotOpened != null)
                             {
-                                var card = _masterDbContext.card_type.Where(a => a.type == decoded.Event.CardType && a.chain_id == chain_id).FirstOrDefault();
-                                var token = _masterDbContext.chain_tokens.Where(a => a.token_address.Equals(card.token) && a.chain_id == card.chain_id).FirstOrDefault();
-                                var cardNotOpened = _masterDbContext.card_not_opened.Where(a => a.buyer.Equals(decoded.Event.User) && a.card_type.Equals(card.type) && a.contract.Equals(log.Address) && a.token.Equals(token.token_address)).FirstOrDefault();
-                                if (cardNotOpened != null)
-                                {
-                                    cardNotOpened.amount += (int)decoded.Event.NumberOfCards;
-                                    cardNotOpened.updater = "system";
-                                    cardNotOpened.update_time = DateTime.Now;
-                                }
-                                else
-                                {
-                                    var notOpened = new card_not_opened() { card_type = card.type, card_name = card.name, amount = (int)decoded.Event.NumberOfCards, buyer = decoded.Event.User, chain_id = chain_id, contract = log.Address, create_time = DateTime.Now, creator = "system", price = card.price, token = card.token, img = card.img };
-                                    _masterDbContext.card_not_opened.Add(notOpened);
-                                }
-                                _masterDbContext.SaveChanges();
+                                cardNotOpened.amount += (int)decoded.Event.NumberOfCards;
+                                cardNotOpened.updater = "system";
+                                cardNotOpened.update_time = DateTime.Now;
                             }
                             else
                             {
-                                Console.WriteLine("CardPurchased:Found not standard log");
+                                var notOpened = new card_not_opened() { card_type = card.type, card_name = card.name, amount = (int)decoded.Event.NumberOfCards, buyer = decoded.Event.User, chain_id = chain_id, contract = log.Address, create_time = DateTime.Now, creator = "system", price = card.price, token = card.token, img = card.img };
+                                _masterDbContext.card_not_opened.Add(notOpened);
                             }
-                        });
-                        await _client.StartAsync();
-                        await _subscription.SubscribeAsync(cardPurchased);
+                            _masterDbContext.SaveChanges();
+                        }
+                        else
+                        {
+                            Console.WriteLine("CardPurchased:Found not standard log");
+                        }
+                    });
+                    await _client.StartAsync();
+                    await _subscription.SubscribeAsync(cardPurchased);
 
-                        // 在这里保持连接处于活动状态，可以根据需要添加逻辑
-                        await Task.Delay(Timeout.Infinite);
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine($"WebSocket Error: {ex}");
-                    }
+                    //while (true)
+                    //{
+                    //    if (_client.WebSocketState == WebSocketState.Open)
+                    //    {
+
+                    //    }
+                    //    else
+                    //    {
+                    //        Console.WriteLine("状态" + _client.WebSocketState);
+                    //        await _client.StartAsync();
+                    //    }
+                    //}
+                }
+                catch (Exception ex)
+                {
+                    await StartAsync(nodeUrl, contractAddress, chain_id);
+                    Console.WriteLine($"WebSocket Error: {ex}");
                 }
             }
+
             catch (Exception ex)
             {
                 await StartAsync(nodeUrl, contractAddress, chain_id);
