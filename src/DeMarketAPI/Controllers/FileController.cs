ba8a34620c0bc459a846af1b878743b79586936b
SouvernirLiu <songnian.liu@outlook.com>
2023-12-05 11:18:41 +0800

    修改

--- src/DeMarketAPI/Controllers/FileController.cs
+++ src/DeMarketAPI/Controllers/FileController.cs
@@ -7,9 +7,10 @@ using Microsoft.Extensions.Hosting;
 using System;
 using System.IO;
 using System.Threading.Tasks;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.Text;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Formats;
 
 namespace FileUploadExample.Controllers
 {
@@ -67,31 +68,8 @@ namespace FileUploadExample.Controllers
                 string grandparentDirectory = Directory.GetParent(Directory.GetParent(_environment.ContentRootPath).FullName).FullName;
                 var uploadDirectory = Path.Combine(grandparentDirectory, "docs/compress"); // 修改为你选择的目录
                 var filePath = Path.Combine(uploadDirectory, fileName);
-                ImageFormat imageFormat = ImageFormat.Jpeg;
-                string fileExtension = Path.GetExtension(fileName).TrimStart('.').ToLower(); ; ;
-                switch (fileExtension)
-                {
-                    case "png":
-                        imageFormat = ImageFormat.Png;
-                        break;
-                    case "gif":
-                        imageFormat = ImageFormat.Gif;
-                        break;
-                    case "ico":
-                        imageFormat = ImageFormat.Icon;
-                        break;
-                    case "jpeg":
-                        imageFormat = ImageFormat.Jpeg;
-                        break;
-                    case "webp":
-                        imageFormat = ImageFormat.Webp;
-                        break;
-                    case "jpg":
-                        imageFormat = ImageFormat.Jpeg;
-                        break;
-                }
-              
-                CompressAndSaveImage(file.OpenReadStream(), filePath, imageFormat);
+                string fileExtension = Path.GetExtension(fileName).TrimStart('.').ToLower();
+                CompressJpegQuality(file.OpenReadStream(), filePath);
                 fileName = _configuration["ApiDomain"] + "/docs/compress/" + fileName;
                 return Json(new WebApiResult(1, "上传图片", fileName));
             }
@@ -100,36 +78,47 @@ namespace FileUploadExample.Controllers
                 return Json(new WebApiResult(-1, "没有上传的问题"));
             }
         }
-        private void CompressAndSaveImage(Stream imageStream, string savePath, ImageFormat imageFormat)
+        private void CompressJpegQuality(Stream imageStream, string outputPath)
         {
-            using (Image image = Image.FromStream(imageStream))
+            var quality = 100;
+            if (imageStream.Length < 200 * 1024) // 将 KB 转换为字节进行比较
             {
-                // 设置压缩质量，这里设置为50（范围从0到100，100为最高质量）
-                EncoderParameters encoderParameters = new EncoderParameters(1);
-                encoderParameters.Param[0] = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, 50L);
-
-                // 获取 JPEG 图像编码器
-                ImageCodecInfo jpegEncoder = GetEncoder(imageFormat);
-
-                // 保存压缩版图片
-                image.Save(savePath, jpegEncoder, encoderParameters);
+                quality = 100;
             }
+            else if (imageStream.Length < 400 * 1024) // 将 KB 转换为字节进行比较
+            {
+                quality = 80;
+            }
+            else if (imageStream.Length < 600 * 1024) // 将 KB 转换为字节进行比较
+            {
+                quality = 70;
+            }
+            else if (imageStream.Length < 800 * 1024) // 将 KB 转换为字节进行比较
+            {
+                quality = 60;
+            }
+            else if (imageStream.Length < 1000 * 1024) // 将 KB 转换为字节进行比较
+            {
+                quality = 50;
+            }
+            else if (imageStream.Length < 2000 * 1024) // 将 KB 转换为字节进行比较
+            {
+                quality = 40;
+            }
+            else
+            {
+                quality = 30;
+            }
+            imageStream.Seek(0, SeekOrigin.Begin); // 将流的位置移回到开头
+                                                   // 
+            using (Image image = Image.Load(imageStream))
+            {
+                var encoder = new JpegEncoder { Quality = quality };
+                // 保存图像到指定路径并应用压缩质量
+                image.Save(outputPath, encoder);
+            }
+
         }
 
-        private ImageCodecInfo GetEncoder(ImageFormat format)
-        {
-            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageDecoders();
-
-            foreach (ImageCodecInfo codec in codecs)
-            {
-                if (codec.FormatID == format.Guid)
-                {
-                    return codec;
-                }
-            }
-
-            // 如果找不到指定格式的编码器，可以返回默认的 JPEG 编码器
-            return codecs[1]; // 这里假设索引为1的编码器是 JPEG 编码器
-        }
     }
 }
