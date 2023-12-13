ee28f86dbf0ae6ccba502cedb95333cafb3b9b69
SouvernirLiu <songnian.liu@outlook.com>
2023-12-13 10:36:15 +0800

    修改

---
+++ src/ListenService/Model/SetOrderInfoEventDTO.cs
@@ -0,0 +1,22 @@
+﻿// 定义一个DTO类，用于解析事件数据
+using System.Numerics;
+using Nethereum.ABI.FunctionEncoding.Attributes;
+namespace ListenService.Model
+{
+    [Event("SetOrderInfo")]
+    public class SetOrderInfoEventDTO : IEventDTO
+    {
+        [Parameter("address", "defaulter", 1, true)]
+        public string Defaulter { get; set; }
+        [Parameter("uint256", "orderId", 2, true)]
+        public string OrderId { get; set; }
+        [Parameter("uint8", "status", 3, true)]
+        public BigInteger Status { get; set; }
+        [Parameter("address", "seller", 4, false)]
+        public BigInteger Seller { get; set; }
+        [Parameter("address", "buyer", 4, false)]
+        public BigInteger Buyer { get; set; }
+
+    }
+}
+
