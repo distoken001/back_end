892bfd2f340b9c1d0ffb0bc3a12b5b3b6d48052d
SouvernirLiu <songnian.liu@outlook.com>
2023-12-13 13:39:38 +0800

    Update AddOrder.cs

--- src/ListenService/Repository/Implements/Ebay/AddOrder.cs
+++ src/ListenService/Repository/Implements/Ebay/AddOrder.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Threading.Tasks;
-using Nethereum.Web3;
-using Nethereum.Web3.Accounts;
-using Nethereum.Contracts;
-using Nethereum.RPC.Eth.DTOs;
+﻿using Nethereum.Contracts;
 using CommonLibrary.DbContext;
-using Newtonsoft.Json.Linq;
-using Nethereum.JsonRpc.WebSocketClient;
 using Nethereum.JsonRpc.WebSocketStreamingClient;
 using Nethereum.RPC.Reactive.Eth.Subscriptions;
 using Newtonsoft.Json;
@@ -19,6 +12,7 @@ using Nethereum.JsonRpc.Client;
 using CommonLibrary.Model.DataEntityModel;
 using CommonLibrary.Common.Common;
 using ListenService.Repository.Interfaces;
+using System.Net.WebSockets;
 
 namespace ListenService.Repository.Implements
 {
@@ -35,44 +29,35 @@ namespace ListenService.Repository.Implements
         {
             try
             {
+                StreamingWebSocketClient.ForceCompleteReadTotalMilliseconds = Timeout.Infinite;
+                //StreamingWebSocketClient.ConnectionTimeout = Timeout.InfiniteTimeSpan;
                 var client = new StreamingWebSocketClient(nodeUrl);
 
                 var addOrder = Event<AddOrderEventDTO>.GetEventABI().CreateFilterInput();
-
                 var subscription = new EthLogsObservableSubscription(client);
+                Action<Exception> onErrorAction = async (ex) =>
+                {
+                    // 处理异常情况 ex
+                    Console.WriteLine($"Error AddOrder: {ex}");
+                    await StartAsync(nodeUrl, contractAddress, chain_id);
+                };
                 // attach a handler for Transfer event logs
                 subscription.GetSubscriptionDataResponsesAsObservable().Subscribe(log =>
                 {
-                    try
-                    {
-                        // decode the log into a typed event log
-                        var decoded = Event<AddOrderEventDTO>.DecodeEvent(log);
-                        if (decoded != null && log.Address.Equals(contractAddress, StringComparison.OrdinalIgnoreCase))
-                        {
+                    // decode the log into a typed event log
+                   
+                }, onErrorAction);
 
-
-                        }
-                        else
-                        {
-
-                            Console.WriteLine("AddOrder:Found not standard log");
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine("AddOrder:Log Address: " + log.Address + " is not a standard log:", ex.Message);
-                    }
-                });
-                // open the web socket connection
                 await client.StartAsync();
 
-                // begin receiving subscription data
-                // data will be received on a background thread
                 await subscription.SubscribeAsync(addOrder);
+              
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.ToString());
+                await StartAsync(nodeUrl, contractAddress, chain_id);
+                Console.WriteLine($"AddOrder:{ex}");
+                Console.WriteLine("AddOrder重启了EX");
             }
         }
 
