9e99d4a1300238ae1928b48f680548f4f04ba539
SouvernirLiu <songnian.liu@outlook.com>
2023-12-13 16:27:12 +0800

    修改

--- src/ListenService/Repository/Implements/Ebay/AddOrder.cs
+++ src/ListenService/Repository/Implements/Ebay/AddOrder.cs
@@ -18,6 +18,9 @@ using Nethereum.Web3;
 using Nethereum.RPC;
 using Org.BouncyCastle.Asn1.X509;
 using Microsoft.VisualBasic;
+using Microsoft.AspNetCore.Mvc;
+using Telegram.Bot;
+using Microsoft.EntityFrameworkCore;
 
 namespace ListenService.Repository.Implements
 {
@@ -75,10 +78,12 @@ namespace ListenService.Repository.Implements
                         var orderResult = await function.CallDeserializingToObjectAsync<OrderDTO>((int)decoded.Event.OrderId);
                         var chainToken = _masterDbContext.chain_tokens.Where(a => a.token_address.Equals(orderResult.Token) && a.chain_id == chain_id).FirstOrDefault();
                         var decimals_num = (double)Math.Pow(10, chainToken.decimals);
-                        var order = new orders() { amount = (double)orderResult.Amount, buyer = orderResult.Buyer, buyer_contact = null, buyer_ex = (double)orderResult.BuyerEx/decimals_num, buyer_pledge = (double)orderResult.BuyerPledge, chain_id = chain_id, contract = contractAddress, create_time = DateTime.Now, creator = "system", description = orderResult.Description, img = orderResult.Img, name = orderResult.Name, seller = orderResult.Seller, order_id = (int)decoded.Event.OrderId, price = (double)orderResult.Price / decimals_num, seller_contact = null, seller_pledge = (double)orderResult.SellerPledge/decimals_num, status = orderResult.Status, token = orderResult.Token, updater = null, update_time = DateTime.Now, weight = 10000 };
+                        var order = new orders() { amount = (double)orderResult.Amount, buyer = orderResult.Buyer, buyer_contact = null, buyer_ex = (double)orderResult.BuyerEx / decimals_num, buyer_pledge = (double)orderResult.BuyerPledge, chain_id = chain_id, contract = contractAddress, create_time = DateTime.Now, creator = "system", description = orderResult.Description, img = orderResult.Img, name = orderResult.Name, seller = orderResult.Seller, order_id = (int)decoded.Event.OrderId, price = (double)orderResult.Price / decimals_num, seller_contact = null, seller_pledge = (double)orderResult.SellerPledge / decimals_num, status = orderResult.Status, token = orderResult.Token, updater = null, update_time = DateTime.Now, weight = 10000 };
                         _masterDbContext.orders.Add(order);
                         _masterDbContext.SaveChanges();
-                     
+                        _ = sendBotAuction((int)decoded.Event.OrderId, chain_id, contractAddress);
+
+
                     }
                     else
                     {
@@ -100,6 +105,90 @@ namespace ListenService.Repository.Implements
             }
         }
 
+        private async Task sendBotAuction(long order_id, ChainEnum chain_id, string contract)
+        {
+
+            try
+            {
+                List<long> ls = new List<long>();
+                var order = await _masterDbContext.orders_auction.FirstOrDefaultAsync(p => p.order_id == order_id && p.chain_id == chain_id && p.contract == contract);
+                OrderAuctionStatus status = order.status;
+                var seller = await _masterDbContext.users.FirstOrDefaultAsync(u => u.address == order.seller);
+                var buyer = await _masterDbContext.users.FirstOrDefaultAsync(u => u.address == order.buyer);
+
+                var token = _masterDbContext.chain_tokens.AsNoTracking().FirstOrDefault(c => c.chain_id == order.chain_id && c.token_address.Equals(order.token, StringComparison.OrdinalIgnoreCase));
+
+                var botClient = new TelegramBotClient(_configuration["BotToken"]);
+                string mailMessageSeller = "";
+                string mailMessageBuyer = "";
+                if (status == OrderAuctionStatus.Initial)
+                {
+                    mailMessageSeller = $"您在{order.chain_id.ToString()}链上发布了拍卖商品：{order.name}。";
+                    var chatMessage = $"拍卖订单：用户 @{seller?.nick_name} 在{order.chain_id.ToString()}链上发布了新商品：{order.name}，起拍单价：{order.price} {token.token_name}, 数量：{order.amount}，订单链接:{_configuration["Domain"]}/auction/detail/{order.contract}/{(int)order.chain_id}/{order.order_id}"; ;
+                    await botClient.SendTextMessageAsync(_configuration["GroupChatID"], chatMessage);
+
+                    var chatIDs = _configuration["GroupChatIDs"].Split(',');
+
+                    foreach (var chatID in chatIDs)
+                    {
+                        if (chatID == _configuration["GroupChatID"])
+                        {
+                            continue;
+                        }
+                        if (_configuration[chatID] == token.token_name || token.token_name == "USDT")
+                        {
+                            var message = await botClient.SendTextMessageAsync(long.Parse(chatID), chatMessage);
+                        }
+                    }
+                }
+                else if (status == OrderAuctionStatus.SellerCancelWithoutDuty)
+                {
+                    if (seller?.telegram_id != null)
+                    {
+                        mailMessageSeller = $"您在{order.chain_id.ToString()}链上发布的拍卖商品({order.name})已取消。";
+                    }
+
+                }
+                else if (status == OrderAuctionStatus.Completed)
+                {
+                    mailMessageSeller = $"您在{order.chain_id.ToString()}链上的发布的拍卖商品({order.name})交易已完成。";
+                    mailMessageBuyer = $"您在{order.chain_id.ToString()}链上拍下的商品({order.name})交易已完成。";
+                }
+                else if (status == OrderAuctionStatus.ConsultCancelCompleted)
+                {
+                    mailMessageSeller = $"您在{order.chain_id.ToString()}链上的发布的拍卖商品({order.name})协商取消已完成。";
+                    mailMessageBuyer = $"您在{order.chain_id.ToString()}链上拍下的商品({order.name})协商取消已完成。";
+                }
+                else
+                {
+                    mailMessageSeller = $"您在{order.chain_id.ToString()}链上发布的拍卖商品（{order.name}）有新动态，请及时查看。\n对方Telegram：@" + buyer?.nick_name;
+                    mailMessageBuyer = $"您在{order.chain_id.ToString()}链上拍下的商品（{order.name}）有新动态，请及时查看。\n对方Telegram： @" + seller?.nick_name;
+                }
+                if (!string.IsNullOrEmpty(mailMessageSeller))
+                {
+
+                    var chatId = seller?.telegram_id; // 替换为您要发送消息的聊天ID
+                    if (chatId != null)
+                    {
+                        var message = await botClient.SendTextMessageAsync(chatId, mailMessageSeller);
+                    }
+                }
+                if (!string.IsNullOrEmpty(mailMessageBuyer))
+                {
+                    var chatId = buyer?.telegram_id; // 替换为您要发送消息的聊天ID
+                    if (chatId != null)
+                    {
+                        var message = await botClient.SendTextMessageAsync(chatId, mailMessageBuyer);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception caught in sendBotAuction(): {0}", ex.ToString());
+
+            }
+
+        }
     }
 }
 
