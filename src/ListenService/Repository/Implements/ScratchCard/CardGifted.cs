6055185a2cd42c4c9987c59e01634454c913b803
SouvernirLiu <songnian.liu@outlook.com>
2023-12-21 10:07:24 +0800

    Update CardGifted.cs

--- src/ListenService/Repository/Implements/ScratchCard/CardGifted.cs
+++ src/ListenService/Repository/Implements/ScratchCard/CardGifted.cs
@@ -1,25 +1,12 @@
-﻿using System;
-using System.Threading.Tasks;
-using Nethereum.Web3;
-using Nethereum.Web3.Accounts;
-using Nethereum.Contracts;
-using Nethereum.RPC.Eth.DTOs;
+﻿using Nethereum.Contracts;
 using CommonLibrary.DbContext;
-using Newtonsoft.Json.Linq;
-using Nethereum.JsonRpc.WebSocketClient;
 using Nethereum.JsonRpc.WebSocketStreamingClient;
 using Nethereum.RPC.Reactive.Eth.Subscriptions;
-using Newtonsoft.Json;
-using System;
 using System.Reactive.Linq;
-using System.Threading.Tasks;
-using Nethereum.ABI.Model;
 using ListenService.Model;
-using Nethereum.JsonRpc.Client;
 using CommonLibrary.Model.DataEntityModel;
 using CommonLibrary.Common.Common;
 using ListenService.Repository.Interfaces;
-using System.Net.WebSockets;
 
 namespace ListenService.Repository.Implements
 {
