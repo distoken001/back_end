a66e971c0a61c01819606cfe137303fd389def59
SouvernirLiu <songnian.liu@outlook.com>
2024-12-09 20:36:47 +0800

    Create DeBoxController.cs

---
+++ src/DeMarketAPI/Controllers/DeBoxController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using CommonLibrary.Common.Common;
+using CommonLibrary.DbContext;
+using CommonLibrary.Model.DataEntityModel;
+using DeMarketAPI.Common.Model.HttpApiModel.RequestModel;
+using DeMarketAPI.Common.Model.HttpApiModel.ResponseModel;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace DeMarketAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeBoxController :BaseController
+	{
+        [HttpPost("receivemessage")]
+        [AllowAnonymous]
+        public async Task<JsonResult> ReceiveMessage([FromBody] GetCardTypeListRequest req)
+        {
+          
+            return Json(new WebApiResult(1, "成功" ));
+        }
+    }
+}
+
