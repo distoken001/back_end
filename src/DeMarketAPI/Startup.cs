27abccbacb78a39f8a03327ca431fe2dae669eea
souvernir <songnian.liu@outlook.com>
2025-10-23 16:12:01 +0800

    修改

--- src/DeMarketAPI/Startup.cs
+++ src/DeMarketAPI/Startup.cs
@@ -1,4 +1,6 @@
-﻿using Com.Ctrip.Framework.Apollo;
+﻿using System.IO;
+using System.Text;
+using Com.Ctrip.Framework.Apollo;
 using CommonLibrary.DbContext;
 using CommonLibrary.Model;
 using DeMarketAPI.Proxies;
@@ -17,8 +19,6 @@ using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-using System.IO;
-using System.Text;
 
 namespace DeMarketAPI
 {
@@ -27,13 +27,19 @@ namespace DeMarketAPI
         public Startup(IConfiguration configuration, IHostEnvironment env)
         {
             //输出debug日志在控制台，方便查找问题
-            Com.Ctrip.Framework.Apollo.Logging.LogManager.UseConsoleLogging(Com.Ctrip.Framework.Apollo.Logging.LogLevel.Warning);
+            Com.Ctrip.Framework.Apollo.Logging.LogManager.UseConsoleLogging(
+                Com.Ctrip.Framework.Apollo.Logging.LogLevel.Warning
+            );
             var builder = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
-                //.AddApollo(configuration.GetSection("apollo"))
-                //.AddNamespace("backend.share")
-                //.AddDefault();
+                .AddJsonFile(
+                    $"appsettings.{env.EnvironmentName}.json",
+                    optional: true,
+                    reloadOnChange: true
+                );
+            //.AddApollo(configuration.GetSection("apollo"))
+            //.AddNamespace("backend.share")
+            //.AddDefault();
             Configuration = builder.Build();
         }
 
@@ -50,16 +56,23 @@ namespace DeMarketAPI
                 x.MultipartBodyLengthLimit = int.MaxValue;
                 x.MemoryBufferThreshold = int.MaxValue;
             });
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer((options) => options.TokenValidationParameters = new TokenValidationParameters()
-            {
-                ValidateIssuer = true,
-                ValidateAudience = true,
-                ValidateLifetime = true,
-                ValidateIssuerSigningKey = true,
-                ValidIssuer = StringConstant.issuer,
-                ValidAudience = StringConstant.audience,
-                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(StringConstant.secretKey))
-            });
+            services
+                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(
+                    (options) =>
+                        options.TokenValidationParameters = new TokenValidationParameters()
+                        {
+                            ValidateIssuer = true,
+                            ValidateAudience = true,
+                            ValidateLifetime = true,
+                            ValidateIssuerSigningKey = true,
+                            ValidIssuer = StringConstant.issuer,
+                            ValidAudience = StringConstant.audience,
+                            IssuerSigningKey = new SymmetricSecurityKey(
+                                Encoding.UTF8.GetBytes(StringConstant.secretKey)
+                            ),
+                        }
+                );
             services.AddAuthorization();
             EncodingProvider provider = CodePagesEncodingProvider.Instance;
             Encoding.RegisterProvider(provider);
@@ -81,22 +94,26 @@ namespace DeMarketAPI
                 //.AddSingleton<ApolloConfigs>()
                 .AddCors(options =>
                 {
-                    options.AddPolicy("CorsPolicy", builder =>
-                    {
-                        builder.SetIsOriginAllowed((x) => true)
-                   .AllowAnyOrigin()
-                   .AllowAnyHeader()
-                   .AllowAnyMethod();
-                    });
+                    options.AddPolicy(
+                        "CorsPolicy",
+                        builder =>
+                        {
+                            builder
+                                .SetIsOriginAllowed((x) => true)
+                                .AllowAnyOrigin()
+                                .AllowAnyHeader()
+                                .AllowAnyMethod();
+                        }
+                    );
                 })
                 //.AddDbContext<MySqlMasterDbContext>(options => options.UseMySql(identityConn))
-                .AddDbContext<MySqlMasterDbContext>(options => options.UseMySql(deMarketConn, builder => builder.EnableRetryOnFailure()))
-                .AddMvc(
-                options =>
+                .AddDbContext<MySqlMasterDbContext>(options =>
+                    options.UseMySql(deMarketConn, builder => builder.EnableRetryOnFailure())
+                )
+                .AddMvc(options =>
                 {
                     options.Filters.Add<ExLogFilter>();
-                }
-                )
+                })
                 .AddJsonOptions(options =>
                 {
                     options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
@@ -108,37 +125,59 @@ namespace DeMarketAPI
             //automappe            services.AddAutoMapper(System.Reflection.Assembly.GetExecutingAssembly());
 
             //Swagger 配置
-            services.AddSwaggerGen(options =>
-            {
-                options.SwaggerDoc("v1", new OpenApiInfo
+            services
+                .AddSwaggerGen(options =>
                 {
-                    Version = "v1",
-                    Title = "DeMarketAPI API"
+                    options.SwaggerDoc(
+                        "v1",
+                        new OpenApiInfo { Version = "v1", Title = "DeMarketAPI API" }
+                    );
+                })
+                .ConfigureSwaggerGen(options =>
+                {
+                    options.IncludeXmlComments(
+                        System.IO.Path.Combine(
+                            Microsoft
+                                .Extensions
+                                .PlatformAbstractions
+                                .PlatformServices
+                                .Default
+                                .Application
+                                .ApplicationBasePath,
+                            "DeMarketAPI.xml"
+                        )
+                    );
                 });
-            })
-             .ConfigureSwaggerGen(options =>
-             {
-                 options.IncludeXmlComments(System.IO.Path.Combine(Microsoft.Extensions.PlatformAbstractions.PlatformServices.Default.Application.ApplicationBasePath, "DeMarketAPI.xml"));
-             });
 
             privider = services.BuildServiceProvider();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, Microsoft.AspNetCore.Hosting.IApplicationLifetime lifetime)
+        public void Configure(
+            IApplicationBuilder app,
+            IWebHostEnvironment env,
+            Microsoft.AspNetCore.Hosting.IApplicationLifetime lifetime
+        )
         {
             app.UseHttpsRedirection();
             string docsDirectory = Path.Combine(env.ContentRootPath, "docs");
+            if (!Directory.Exists(docsDirectory))
+            {
+                Directory.CreateDirectory(docsDirectory);
+            }
+
             //app.UseDirectoryBrowser(new DirectoryBrowserOptions
             //{
             //    FileProvider = new PhysicalFileProvider(Path.Combine(docsDirectory, "uploads")),
             //    RequestPath = "/uploads"
             //});
-            app.UseStaticFiles(new StaticFileOptions()
-            {
-                FileProvider = new PhysicalFileProvider(docsDirectory),
-                RequestPath = new PathString("/docs")//对外的访问路径
-            });
+            app.UseStaticFiles(
+                new StaticFileOptions()
+                {
+                    FileProvider = new PhysicalFileProvider(docsDirectory),
+                    RequestPath = new PathString("/docs"), //对外的访问路径
+                }
+            );
 
             if (env.IsDevelopment())
             {
@@ -155,10 +194,10 @@ namespace DeMarketAPI
             {
                 //Swagger 配置
                 app.UseSwagger()
-              .UseSwaggerUI(c =>
-              {
-                  c.SwaggerEndpoint($"/swagger/v1/swagger.json", "DeMarketAPI");
-              });
+                    .UseSwaggerUI(c =>
+                    {
+                        c.SwaggerEndpoint($"/swagger/v1/swagger.json", "DeMarketAPI");
+                    });
             }
 
             //Swagger 配置
